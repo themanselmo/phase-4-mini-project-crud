d53be5435b70696b97e0b36336b41a48f7cad1d4
max anselmo <itmanselmo@gmail.com>
2021-11-24 14:10:02 -0500

    completed assignment

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,43 @@
+class SpicesController < ApplicationController
+
+    def index 
+        spices = Spice.all 
+        render json: spices
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = Spice.find(params[:id])
+        if spice 
+            spice.update(spice_params)
+            render json: spice
+        else
+            render json: { error: "Spice not found" }, status: :not_found
+        end
+    end
+
+    def destroy
+        spice = Spice.find(params[:id])
+        if spice
+            spice.destroy 
+            head :no_content
+        else
+            render json: { error: "Spice not found" }, status: :not_found
+        end
+    end
+
+    def spice_params
+        params.permit(
+            :title,
+            :image,
+            :description,
+            :notes,
+            :rating
+        )
+    end
+
+end
